19f12c86bf640fcce9d9f6d6149a2b4b2658d5f1
hpsprmpsp <hpsprmpsp@gmail.com>
2018-02-09 15:19:44 +0530

    some changes

---
+++ video/video.js
@@ -0,0 +1,39 @@
+var video=[];
+video.source="";
+video.inp=[];
+video.inp.actual=$("<input type='file' class='hidden' />");
+video.inp.toShow=$("<div class='input-holder'><input readonly class='input-group' data-ride='file-name'/><button class='input-group'>Browse</button></div>");
+
+
+
+
+
+video.clear=function(){
+	video.container.empty();
+}
+
+
+
+
+video.inp.initiate=function(){
+	$('#div-container-input').html(video.inp.actual);
+	$('#div-container-input').append(video.inp.toShow);
+	video.inp.toShow.find('button').on('click',function(){
+		video.inp.actual.click();
+		video.inp.actual.on('change',function(){
+			video.inp.toShow.find('input').val(video.inp.actual.val());
+				video.initiate();
+			});	
+	});
+};
+
+video.inp.clear=function(){
+	$('#div-container-input').empty();
+};
+
+
+
+
+video.initiate=function(){
+
+};
