d356325f72c13617be99200e20bb284d0d116b50
spadhee <Soumya.Padhee@riverbed.com>
2019-01-15 00:46:45 +0530

    setting index page

---
+++ index.js
@@ -0,0 +1,23 @@
+var files_raw = [
+				{name: "BMI", href:"BMI.html"},
+				{name: "Click Me", href: "clickme.html"},
+				{name: "Draw Graph", href: "DrawGraphBasic.html"},
+				{name: "Basic Clock", href: "DigitalClock_Plain.html"},
+				{name: "Calender", href: "Calender.html" },
+				{name: "Calculator", href:"BasicCalculator.html"},
+				{name: "Converter(All)", href:"Converter.html"},
+				{name: "Digital Clock (Time Indicator)", href:"GiantClockDigital.html"},
+				{name: "Hangman", href:"hangman.html"},
+				{name: "Diwali", href:"HappyDiwali.html"},
+				{name: "Valentine wish", href: "hBeats.html"},
+				{name: "HNY 2019", href:"hpn2019.html"},
+				{name: "Loader (Rope Bundle)", href: "Loader1.html"},
+				{name: "Message Container", href:"Message.html"},
+				{name: "Point tracker", href: "Point Tracking Tool.html"},
+				{name: "Morse it up", href: "morseConverter.html"},
+				{name: "Rock-Paper-Scissor", href:"RockPaperScissor.html"},
+				{name: "Roll A dice", href:"Roll-A-Dice.html"},
+				{name: "Text-Binart-Text", href:"TextToBinaryAndViceVersa.html"},
+				{name: "Tic-Tac-Toe", href: "TicTacToe.html"},
+
+			];
